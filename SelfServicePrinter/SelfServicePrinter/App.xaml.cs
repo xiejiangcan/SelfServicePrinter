de76b8f797fb3d1050a1e9550c53e788c9b0c17c
Jiangcan Xie <756795122@qq.com>
2022-12-21 10:38:12 +0800

    完成主页部分内容

---
+++ SelfServicePrinter/SelfServicePrinter/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Ioc;
+using SelfServicePrinter.Common;
+using SelfServicePrinter.ViewModels.Dialogs;
+using SelfServicePrinter.Views;
+using SelfServicePrinter.Views.Dialogs;
+using System.Windows;
+
+namespace SelfServicePrinter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<IDialogHostService, DialogHostService>();
+
+
+            containerRegistry.RegisterForNavigation<MsgView, MsgViewModel>();
+        }
+    }
+}
---
+++ SelfServicePrinter/SelfServicePrinter/Views/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Events;
+using SelfServicePrinter.Common;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SelfServicePrinter.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(IEventAggregator aggregator, IDialogHostService dialogHostService)
+        {
+            InitializeComponent();
+
+            btnMin.Click += (s, e) => { this.WindowState = WindowState.Minimized; };
+            
+            btnClose.Click += async (s, e) =>
+            {
+                var dialogResult = await dialogHostService.Question("温馨提示", "确认退出系统?");
+                if (dialogResult.Result != Prism.Services.Dialogs.ButtonResult.OK) return;
+                this.Close();
+            };
+            ColorZone.MouseMove += (s, e) =>
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                    this.DragMove();
+            };
+        }
+    }
+}
